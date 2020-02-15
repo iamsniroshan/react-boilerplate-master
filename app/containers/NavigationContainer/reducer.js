a03ef73f415dfdac6729ee0a5de5982f9e0aa75d
Niroshan <iamsniroshan@gmail.com>
2020-02-15 23:39:17 +0530

    issue here

---
+++ app/containers/NavigationContainer/reducer.js
@@ -0,0 +1,20 @@
+/*
+ *
+ * NavigationContainer reducer
+ *
+ */
+import produce from 'immer';
+import { DEFAULT_ACTION } from './constants';
+
+export const initialState = {};
+
+/* eslint-disable default-case, no-param-reassign */
+const navigationContainerReducer = (state = initialState, action) =>
+  produce(state, (/* draft */) => {
+    switch (action.type) {
+      case DEFAULT_ACTION:
+        break;
+    }
+  });
+
+export default navigationContainerReducer;
